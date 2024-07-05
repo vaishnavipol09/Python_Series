b16fe2ad89a867d4d92a31d2511daa88c7b747ae
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-05 15:23:13 +0530

    Solution_5

---
+++ 02_conditionals/05_Solution.py
@@ -0,0 +1,10 @@
+Weather = "Sunny"
+
+if Weather == "sunny":
+    print("Go for a walk")
+elif Weather == "Rainy":
+    print("Read a book")
+elif Weather == "Snow":
+    print("Build a snowman")
+else:
+    print("Stay home and relax")            
