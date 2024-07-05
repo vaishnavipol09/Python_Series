25539482e349746c21ec2eb4fe921550922cba8c
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-05 15:46:27 +0530

    Solution_7

---
+++ 02_conditionals/07_Solution.py
@@ -0,0 +1,12 @@
+ordeSize = "Medium"
+extraShot = "True"
+
+if extraShot:
+    if ordeSize == "Small":
+        print("small")
+    elif ordeSize == "medium":
+        print("Medium")
+    elif ordeSize == "Large":
+        print("Large")
+    else:
+        print("Invalid order size")    
