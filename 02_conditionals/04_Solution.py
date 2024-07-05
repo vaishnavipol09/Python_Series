c3e93e253ac5846ef3f4fc118bb161476c3b9e8e
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-05 15:09:35 +0530

    Solution_4

---
+++ 02_conditionals/04_Solution.py
@@ -0,0 +1,11 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana" :
+    if color == "Yellow":
+         print("The color of the banana is ripe.")
+    elif color == "Green":
+         print("The color of the banana is unripe") 
+    elif color == "Brown":
+         print("The color of the banana is Overripe")         
+
