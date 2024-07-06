c27eb4c4ef9e70b816a5595f3beaedaf9acc656f
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-06 16:14:51 +0530

    Solution_6

---
+++ 03_loops/06_Solution.py
@@ -0,0 +1,10 @@
+number = 5
+factorial = 1
+
+while number >=1:
+    #factorial = factorial * number
+    #number = number - 1
+
+    factorial *= number
+    number -= 1
+print("factorial value is:" , factorial) 
