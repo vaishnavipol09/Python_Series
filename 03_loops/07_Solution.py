612bd848ed3ec2f8f857871dadb56b19da9fdd79
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-06 16:23:25 +0530

    Solution_7

---
+++ 03_loops/07_Solution.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter the number between 1 to 10: "))
+    if 1<= number <=10:
+        print("Valid number")
+        break
+    else:
+        print("Invalid number. Please try again.")
